9cd17fb8ca5e090d73f69c3ea1b0898a9b5d11e7
Chrisphine10 <ondiekchrisphine@gmail.com>
2020-05-23 21:44:21 +0300

    adding users module with device

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,19 @@
 
 class PostsController < ApplicationController
   before_action :find_post, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   def index
     @posts = Post.all
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       redirect_to @post
